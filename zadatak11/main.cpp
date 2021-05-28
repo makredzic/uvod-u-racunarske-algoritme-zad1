4437cb5b3a40d518cbfb4b9f49d22aa5a73e140d
makredzic <mak.redzic@gmail.com>
2021-05-28 20:53:08 +0200

    Zavrsen 11. zadatak.

--- zadatak11/main.cpp
+++ zadatak11/main.cpp
@@ -1,6 +1,17 @@
 #include "ura.hpp"
+#include <vector>
+#include <iostream>
 
 // U main datoteci mozete testirati vas kod na proizvoljan nacin.
 
 int main(int argc, char* argv[]) {
+
+	std::vector<int> v{1, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5};
+	insertionsort(v.begin(), v.end());
+
+	for (int i = 0; i < v.size(); i++) std::cout << v[i] << " ";
+	std::cout << std::endl;
+
+
+	return 0;
 }
--- zadatak11/ura.hpp
+++ zadatak11/ura.hpp
@@ -4,8 +4,46 @@
 
 // U ovoj datoteci se nalazi funkcija koju trebate implementirati
 
-// TODO Vas kod ovdje
 template <typename It>
 void insertionsort(It begin, It end) {
+	if (begin == end) return;
+
+
+
+	// marker2 pa marker1 u nizu
+	// border je element koji dijeli sortirani dio niza od nesortiranog
+
+	It marker1 = begin; 
+	It marker2 = marker1++;
+
+	It border = marker1;
+
+	auto current = *begin;
+
+	while (border != end) {
+		
+		//kopija elementa koji treba da se smjesti u sortirani dio niza
+		current = *marker1;
+
+
+		// glavna petlja koja pomijera markere
+		// ili do pocetka niza
+		// ili dok ne naidje na element koji nije manji od current elementa
+		while (marker1 != begin && current < *marker2) {
+			*(marker1--) = *(marker2--);
+		}
+		
+		// stavljanje izabranog elementa na odgovarajucu lokaciju u sortiranom dijelu niza
+		*marker1 = current;
+		
+		//resetovanje markera na granicu
+		//za sljedecu iteraciju
+		marker2 = border++;
+		marker1 = border;
+		
+	}
+
+
+		
 }
 
