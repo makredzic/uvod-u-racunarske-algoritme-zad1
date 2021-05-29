956f74277cdd3193d60bce374c7b0687f2215b02
makredzic <mak.redzic@gmail.com>
2021-05-29 07:31:52 +0200

    Zavrsen 12. zadatak.

--- zadatak12/main.cpp
+++ zadatak12/main.cpp
@@ -1,6 +1,16 @@
 #include "ura.hpp"
+#include <vector>
+#include <iostream>
 
 // U main datoteci mozete testirati vas kod na proizvoljan nacin.
 
 int main(int argc, char* argv[]) {
+
+	std::vector<int> v{5,4,3,2,1,0,-1,-2,-3,-4,-5};
+	shellsort(v.begin(), v.end());
+
+	for (int i = 0; i < v.size(); i++) std::cout << v[i] << " ";
+	std::cout << std::endl;
+
+	return 0;
 }
--- zadatak12/ura.hpp
+++ zadatak12/ura.hpp
@@ -1,11 +1,31 @@
 #pragma once
+#include <iostream> // REMOVE THIS
 
 #include <algorithm>
 
 // U ovoj datoteci se nalazi funkcija koju trebate implementirati
+//
 
-// TODO Vas kod ovdje
+template<typename It>
+static void insertion_k(It begin, int n, int k) {
+
+	for (int i = k; i < n; i+= k ) {
+		for (int j = i; j >= k && *(begin+j) < *(begin+j-k); j-=k) {
+			auto temp = *(begin+j);
+			*(begin+j) = *(begin+j-k);
+			*(begin+j-k) = temp;
+		}
+	}
+	
+
+}
+
 template <typename It>
 void shellsort(It begin, It end) {
+	int n = end - begin;
+	for (int k = n/2; k != 1; k/=2)  {
+		for (int i = 0; i < k; i++) insertion_k(begin+i, n-i, k);
+	}
+	insertion_k(begin, n, 1);
 }
 
