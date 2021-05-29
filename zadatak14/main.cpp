0f88de9fdffd26855dd56e73cd9a90c99b8ad282
makredzic <mak.redzic@gmail.com>
2021-05-29 11:04:47 +0200

    Zavrsen 14. zadatak.

--- zadatak14/main.cpp
+++ zadatak14/main.cpp
@@ -1,6 +1,22 @@
 #include "ura.hpp"
+#include <vector>
+#include <iostream>
 
 // U main datoteci mozete testirati vas kod na proizvoljan nacin.
 
 int main(int argc, char* argv[]) {
+
+	std::vector<int> v{4,-1,5,6,0,-2,-4,-3,2};
+	//                 4 -1 5 6 2 -2 -4 -3 0
+	//                 -3 -1 5 6 2 -2 -4 4 0
+	//                 -3 -1 -4 6 2 -2 5 4 0
+	//                 -3 -1 -4 -2 2 6 5 4 0
+
+	quicksort(v.begin(), v.end());
+
+	for (int i = 0; i < v.size(); i++) std::cout << v[i] << " ";
+	std::cout << std::endl;
+
+
+	return 0;
 }
--- zadatak14/ura.hpp
+++ zadatak14/ura.hpp
@@ -2,10 +2,74 @@
 
 #include <algorithm>
 
-// U ovoj datoteci se nalazi funkcija koju trebate implementirati
+template <typename It>
+static void insertionsort(It begin, It end) {
+	if (begin == end) return;
 
-// TODO Vas kod ovdje
+	It marker1 = begin;
+	It marker2 = marker1++;
+
+	It border = marker1;
+	
+	while (border != end) {
+		auto current = *marker1;
+
+		while (marker1 != begin && current < *marker2){
+			*(marker1--) = *(marker2--);
+		}
+
+		*marker1 = current;
+
+		marker2 = border++;
+		marker1 = border;
+
+	}
+
+}
+
+template<typename It, typename F>
+static It my_partition(It begin, It end, F f) {
+
+	while (begin < end) {
+		if (f(*begin)) {
+			++begin;		
+		} else 
+
+		if (!f(*(end-1))) {
+			--end;
+		} else {
+			auto temp = *begin;
+			*begin = *(end-1);
+			*(end-1) = temp;
+		}
+	}
+	
+	return begin;
+}	
+
+
 template <typename It>
 void quicksort(It begin, It end) {
+	if (end-begin <= 10) {
+		return insertionsort(begin, end);
+	}
+
+	It middle = begin + (end-begin)/2;
+
+	auto temp = *middle;
+	*middle= *(end-1);
+	*(end-1) = temp;
+
+	It pivot = my_partition(begin, end-1, [&](const auto& element) {
+			return element < *(end-1);
+	});
+	
+	temp = *pivot;
+	*pivot = *(end-1);
+	*(end-1) = temp;
+
+	quicksort(begin, pivot);
+	quicksort(pivot+1, end);
+
 }
 
